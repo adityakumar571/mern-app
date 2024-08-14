aa3ca6d115dd5a2a82bcc0e11a4996043c5c7021
Aditya kumar <adityakumar.email@gmail.com>
2024-08-14 17:35:21 +0530

    testing

--- clientSide/src/pages/Home.jsx
+++ clientSide/src/pages/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
 
   const fetchProducts = async () => {
     try {
-      const url = "https://localhost:8080/products";
+      const url = "https://mern-app-kohl.vercel.app/products";
       const headers = {
         headers: {
           'Authorization': localStorage.getItem('token'),
--- clientSide/src/pages/Login.jsx
+++ clientSide/src/pages/Login.jsx
@@ -32,7 +32,7 @@ function Login() {
     }
 
     try {
-      const url = "https://localhost:8080/auth/login";
+      const url = "https://mern-app-kohl.vercel.app/auth/login";
       const response = await fetch(url, {
         method: "POST",
         headers: {
